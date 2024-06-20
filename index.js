4d61fb006b2343346d9815f088665ea9f0c82024
Mac <mac@Ronald.local>
2024-06-21 05:46:02 +1000

    Complete main functions

---
+++ index.js
@@ -0,0 +1,8 @@
+// collect channel points every 5 min
+const interval = setInterval(function() {
+    let button = document.getElementsByClassName("ScCoreButton-sc-ocjdkq-0 ScCoreButtonSuccess-sc-ocjdkq-5 ljgEdo fEpwrH");
+
+    if (button.length > 0) {
+        button[0].click();
+    }
+}, 300000);
